cb21d0e2a2fb67124f6d742cf2583f72b6c357ca
seoywa <132980713+seoywa@users.noreply.github.com>
2025-05-17 16:04:15 +0700

    Implement page routing setup, document editor page, authentication with clerk

---
+++ app/documents/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Share</p>
+        </div>
+
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
+
+export default Document;
